f377228e059cdbdfd367ca6ca2b38cb6dfd7a761
tedo-ghachava <ghachava5@gmail.com>
2021-06-06 19:03:12 +0400

    added homework

---
+++ script.js
@@ -0,0 +1,88 @@
+//დავალება 1
+
+// let cars = ['audi','bmw', 'lexus', 'volkswagen', 'porsche', 'audi']; 
+
+// for( let i=1; i<5; i++) {
+//    console.log(cars[i]);
+// }
+
+
+//დავალება 2
+
+// let names = ['tekle','nino', 'nikolozi', 'gio', 'nia', 'dito']; 
+
+// for (let i=0; i<names.length; i++){
+//     if( names[i].length > 3){
+//                 console.log(names[i]);
+//       }
+//     }
+
+
+// დავალება 3
+// ლუწი
+// let numbers = ['14','33', '27', '18', '38', '123', '44', '71']; 
+// for (let i of numbers){
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+// }
+// კენტი
+// let numbers = ['14','33', '27', '18', '38', '123', '44', '71']; 
+// for (let i of numbers){
+//     if(i % 2 == 1){
+//         console.log(i);
+//     }
+// }
+
+
+// დავალება 4
+// let numbers= [15, 67, 100, 5, 7, [80, 500] ]; 
+
+// console.log(numbers[5][1]);
+
+
+// დავალება 5
+// var person = {
+//     firstname: "john",
+//     lastname: "doe",
+//     age: 50,
+//     eyecolor: "blue"
+//   };
+  
+//   console.log (person.eyecolor);
+// or
+//   console.log(person['eyecolor']);
+  
+
+// დავალება 6
+
+// let languages = ['html', 'css', 'js', 'python', 'c++', 'c#'];
+ 
+//  for (let i=0; i<languages.length; i++){
+//     console.log(languages[i]);
+//  } 
+
+// დავალება 7
+// let names = ['tekle','nino', 'gio', 'dito', 'nika']; 
+
+// if( names.length == 5 && names[names.length-1]=='nika'){
+//     console.log( names.length, names[4]);
+// } else {console.log('error');
+// } 
+
+// 2nd option
+// let names = ['sandro', 'tekle', 'nino', 'gio', 'dito', 'nika']; 
+
+// if( names.length == 7 || names[0]=='sandro'){
+//     console.log('მასივის რაოდენობა: 7 და პირველი ელემენტია სანდრო');
+// } else {console.log('error');
+// } 
+
+
+// დავალება 8
+// let x = 6;
+// let y = '6';
+
+// console.log(x === y);
+
+//დაბრუნებს false რადგან 1 ციფრია(number) ხოლო მეორე ტექსტი(string) და სამი ტოლობის შემთხვევაში ტიპების კონკერტაცია არ მოხდება
